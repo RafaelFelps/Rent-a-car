377361ed9d75a1a91dff3118e9071f481c59a3e0
RafaelFelps <rafaelfelps62@gmail.com>
2021-06-05 13:22:54 -0300

    Config workspace

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World!' });
+})
+app.post('/', (req, res) => {
+    const { name } = req.body;
+    res.json({ name });
+})
+
+app.listen(3333, () => console.log("Server is running"));
