a514a96a103e04c0256084367dc3b8dc3471709b
RafaelFelps <rafaelfelps62@gmail.com>
2021-06-12 14:15:53 -0300

    Create Categories

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express = require("express");
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({ message: "Hello World!" });
-});
-app.post("/", (req, res) => {
-    const { name } = req.body;
-    res.json({ name });
-});
+// Routes
+app.use(categoriesRoutes);
+
+
 
 app.listen(3333, () => console.log("Server is running"));
