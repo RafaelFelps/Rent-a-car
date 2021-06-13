fe039e7b63f776449244e393cf6fda9e927fe24e
RafaelFelps <rafaelfelps62@gmail.com>
2021-06-13 13:33:46 -0300

    Inserting categories

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,6 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use(categoriesRoutes);
-
-
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running"));
